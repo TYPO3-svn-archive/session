8f93f32832859a54f8f250b93386d58b2cdea9c3
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-12 15:32:30 +0000

    Added some contents to documentation. Rise version to 1.2.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/session/trunk@52940 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "session".
 #
-# Auto generated 18-08-2011 13:15
+# Auto generated 12-10-2011 17:27
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Provides userfunctions to set and get session variables in typoscript context.',
 	'category' => 'plugin',
 	'author' => 'Armin Ruediger Vieweg',
-	'author_email' => 'armin.vieweg@diemedialen.de',
-	'author_company' => 'Die Medialen GmbH, www.diemedialen.de',
+	'author_email' => 'info@professorweb.de',
+	'author_company' => '',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'version' => '1.3.0',
+	'version' => '1.2.1',
 	'loadOrder' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"2ed8";s:17:"ext_localconf.php";s:4:"2ec3";s:14:"ext_tables.php";s:4:"9298";s:14:"ext_tables.sql";s:4:"d41d";s:29:"Classes/class.userSession.php";s:4:"b15c";s:28:"Classes/Utility/Settings.php";s:4:"cfbb";s:34:"Configuration/TypoScript/setup.txt";s:4:"d8c1";s:14:"doc/manual.sxw";s:4:"4001";}',
+	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"2ed8";s:17:"ext_localconf.php";s:4:"3bb1";s:14:"ext_tables.php";s:4:"9298";s:14:"ext_tables.sql";s:4:"d41d";s:29:"Classes/class.userSession.php";s:4:"b15c";s:35:"Classes/user_conditionFunctions.php";s:4:"7165";s:28:"Classes/Utility/Settings.php";s:4:"cfbb";s:34:"Configuration/TypoScript/setup.txt";s:4:"d8c1";s:14:"doc/manual.sxw";s:4:"0d5f";}',
 	'suggests' => array(
 	),
 );
